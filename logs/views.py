d47f8dbb24b0bae7ef86e908cf2c90c5730bcf4f
HurmakR <r.hurmak@gmail.com>
2022-12-04 15:58:59 +0200

    first attempt for model forms and views procfile edited

--- logs/views.py
+++ logs/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from logs.models import Model
+from django.views.generic import ListView, TemplateView, DetailView, DeleteView, UpdateView, CreateView
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+class ModelListView(ListView):
+    model = Model
+    #queryset = Model.objects.order_by('name')
